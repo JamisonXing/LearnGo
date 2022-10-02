8a959404e057606978009f1284618370b515f8b4
JackeyXing <2810947907@qq.com>
2022-10-02 22:32:36 +0800

    slice

---
+++ hello/demo2.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	/*	//获取键盘输入字符
+		reader := bufio.NewReader(os.Stdin)
+		readString, _ := reader.ReadString('\n')
+		fmt.Println(readString)*/
+
+	/*	//数组
+		scores := [5]int{1, 2, 3, 4, 5}
+		for index, value := range scores {
+			fmt.Printf("索引：%d, 得分：%d\n", index, value)
+		}
+
+		scores1 := [3]int{2: 3} //只初始化第三个值，且设置为3
+		fmt.Print(scores1)*/
+
+	/*	//多维数组
+		arr := [4][2]int{1: {12, 21}, 2: {43, 34}}
+		for index, value := range arr {
+			fmt.Printf("索引：%d,值：%d\n", index, value)
+		}
+		//结果：
+		//索引：0,值：[0 0]
+		//索引：1,值：[12 21]
+		//索引：2,值：[43 34]
+		//索引：3,值：[0 0]
+
+		//对多维数组元素的简单应用
+		var arr1 [2]int = arr[1]
+		fmt.Print(arr1)
+		var a int = arr[1][0]
+		fmt.Print(a)*/
+
+}
