d3a5874894d69b374ccf2cc96c17fe586d913a73
jamison <2810947907@qq.com>
2022-11-06 20:34:01 +0800

    built-in template function

---
+++ gin-project/chapter03/test_func_tpl.go
@@ -0,0 +1,33 @@
+package chapter03
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+func FuncTpl(ctx *gin.Context) {
+	//fmt.Sprintf()是有返回值的,主要用于格式化，通常用于数据写到数据库或者日志里面
+	user := fmt.Sprintf("name:%s,age:%d,addr:%s", "jamsion", 20, "xxx")
+	fmt.Println(user)
+	name := "jamison"
+	age := 18
+	sliceData := []string{"ZhangSan", "LiSi", "WangWu"}
+
+	mapData := map[string]string{
+		"name": "jamison",
+		"age":  "34",
+	}
+
+	timeNow := time.Now()
+
+	m := map[string]any{
+		"name":      name,
+		"age":       age,
+		"sliceData": sliceData,
+		"mapData":   mapData,
+		"timeNow":   timeNow,
+	}
+	ctx.HTML(http.StatusOK, "chapter03/test_func_tpl.html", m)
+}
