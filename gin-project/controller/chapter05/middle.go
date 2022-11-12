d097603f19ce71d384ecf59f73cad26bd103e795
jamison <2810947907@qq.com>
2022-11-12 15:16:56 +0800

    use next func to calculate time

--- gin-project/controller/chapter05/middle.go
+++ gin-project/controller/chapter05/middle.go
@@ -3,15 +3,20 @@ package chapter05
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 // Middleware01 第一种定义方式
 func Middleware01(ctx *gin.Context) {
+	start_time := time.Now()
+
 	fmt.Println("custom middleware01--start")
 
 	ctx.Next() //执行下一个中间件
 	fmt.Println("custom middleware01--end")
 
+	cost := time.Since(start_time) //计算请求耗时
+	fmt.Println("request time is:", cost)
 }
 
 // Middleware02 第二种定义方式
@@ -19,11 +24,11 @@ func Middleware02() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		fmt.Println("custom middleware02--start")
 
-		if true { //假设不满足某个条件
-			ctx.Abort() //终止调用整个链条
-		}
+		/*		if true { //假设不满足某个条件
+				ctx.Abort() //终止调用整个链条
+			}*/
 		ctx.Next()
-
+		time.Sleep(time.Second * 2)
 		fmt.Println("custom middleware02--end")
 	}
 }
