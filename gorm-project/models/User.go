fda0798f965fc578305130a30e318bbb8f1ee82e
jamison <2810947907@qq.com>
2022-11-13 23:47:10 +0800

    gorm tag

--- gorm-project/models/User.go
+++ gorm-project/models/User.go
@@ -1,10 +1,11 @@
 package models
 
 type User struct {
-	Id    int
-	Name  string
-	Age   int
-	Addr  string
-	Pic   string //头像地址
-	Phone string
+	Id     int    `gorm:"primaryKey"`
+	Name   string `gorm:"index:idx_name;not null"`
+	Age    int
+	Addr   string
+	Pic    string //头像地址
+	Phone  string
+	Ignore string `gorm:"-"`
 }
