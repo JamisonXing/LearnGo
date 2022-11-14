282aa18363a5ea9f199b1c6cd5aa7caeef8a4600
jamison <2810947907@qq.com>
2022-11-14 15:50:49 +0800

    one2one belong/contain

--- gorm-project/ddl_operate.go
+++ gorm-project/ddl_operate.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"gorm-project/models/relate_tables"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
 func main() {
+	//username:password@tcp(ip:port)/dbname?charset=utf8&parseTime=True&loc=Local
+	dsn := "root:xxj753896@tcp(localhost:3306)/gorm_project?charset=utf8mb3&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	//自动迁移
+	db.AutoMigrate(&relate_tables.UserProfile{}, &relate_tables.User{})
 
 }
--- gorm-project/models/relate_tables/one_to_one.go
+++ gorm-project/models/relate_tables/one_to_one.go
@@ -1,8 +1,8 @@
 package relate_tables
 
-//一对一
-
-type User struct {
+//一对一，属于
+//属于：关系和外键在同一方，有关系的哪一方属于另外一个模型
+/*type User struct {
 	Id   int
 	Name string `gorm:"type:varchar(204)"`
 	Age  int
@@ -18,4 +18,24 @@ type UserProfile struct {
 	User User `gorm:"ForeignKey:UId;AssociationForeignKey:Id"` //关联关系
 	//UserID int  //默认关联字段
 	UId int //自定义外键名
+}
+*/
+
+//一对一,包含
+//包含：关系和外键不在同一方，有关系的那一方包含另外一个模型
+
+type User struct {
+	Id   int
+	Name string `gorm:"type:varchar(204)"`
+	Age  int
+	Addr string `gorm:"type:varchar(204)"`
+	PId  int
+}
+
+type UserProfile struct {
+	Id    int
+	Pic   string `gorm:"type:varchar(204)"`
+	CPic  string `gorm:"type:varchar(204)"`
+	Phone string `gorm:"type:varchar(204)"`
+	User  User   `gorm:"ForeignKey:PId;AssociationForeignKey:Id"` //关联关系
 }
