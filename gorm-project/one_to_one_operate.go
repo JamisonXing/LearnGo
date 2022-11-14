48048b387951c6fc61a3127904811181efb9e339
jamison <2810947907@qq.com>
2022-11-14 17:02:56 +0800

    one2one crud

--- gorm-project/one_to_one_operate.go
+++ gorm-project/one_to_one_operate.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"gorm-project/models/relate_tables"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -33,16 +32,33 @@ func main() {
 
 		db.Create(&userProfile)*/
 
-	//关联查询
-	//方式一
-	var userProfile relate_tables.UserProfile
-	db.Debug().First(&userProfile, 1)
-	db.Debug().Model(&userProfile).Association("User").Find(&userProfile.User)
-	fmt.Println(userProfile)
+	/*	//关联查询
+		//方式一
+		var userProfile relate_tables.UserProfile
+		db.Debug().First(&userProfile, 1)
+		db.Debug().Model(&userProfile).Association("User").Find(&userProfile.User)
+		fmt.Println(userProfile)
 
-	//方式二
-	var userProfile2 relate_tables.UserProfile
-	db.Preload("User").Find(&userProfile2, 2)
-	fmt.Println(userProfile2)
+		//方式二
+		var userProfile2 relate_tables.UserProfile
+		db.Preload("User").Find(&userProfile2, 2)
+		fmt.Println(userProfile2)*/
+
+	//更新
+	var userProfile3 relate_tables.UserProfile
+	//先查询
+	db.Preload("User").Find(&userProfile3, 1)
+
+	//db.Model(&userProfile3.User).Update("name", "xxj") //单字段
+	db.Model(&userProfile3.User).Updates(relate_tables.User{ //多字段
+		Name: "lll",
+		Age:  45,
+		Addr: "gg",
+	})
+
+	//删除
+	var userProfile4 relate_tables.UserProfile
+	db.Preload("User").Find(&userProfile4, 2)
+	db.Debug().Delete(&userProfile4.User)
 
 }
